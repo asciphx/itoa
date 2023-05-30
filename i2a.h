9ae7aa6c15415e8e356f41aa95ec66b0a6bb44d6
asciphx <42582375+asciphx@users.noreply.github.com>
2023-05-30 20:10:10 +0800

    optimize

--- i2a.h
+++ i2a.h
@@ -15,8 +15,28 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+  //The fastest h2a fuction
+  char* h2a(char* c, unsigned char i);
+  //The fastest a2a fuction
+  static
+#if defined(_WIN32)
+    __forceinline
+#else
+    inline __attribute__((always_inline))
+#endif
+  char* a2a(char* c, char i) { if (i < 0) { *c = 45; return h2a(++c, ~--i); } return h2a(c, i); }
+  //The fastest t2a fuction
+  char* t2a(char* c, unsigned short i);
+  //The fastest stoa fuction
+  static
+#if defined(_WIN32)
+    __forceinline
+#else
+    inline __attribute__((always_inline))
+#endif
+  char* s2a(char* c, short i) { if (i < 0) { *c = 45; return t2a(++c, ~--i); } return t2a(c, i); }
   //The fastest u2a fuction
-  unsigned char u2a(char* c, unsigned int i);
+  char* u2a(char* c, unsigned int i);
   //The fastest itoa fuction
   static
 #if defined(_WIN32)
@@ -24,11 +44,11 @@ extern "C" {
 #else
   inline __attribute__((always_inline))
 #endif
-  unsigned char i2a(char* c, int i) {
+  char* i2a(char* c, int i) {
   if (i < 0) { *c = 45; return u2a(++c, ~--i); } return u2a(c, i);
   }
   //The fastest u64toa fuction
-  unsigned char u64toa(char* c, unsigned long long i);
+  char* u64toa(char* c, unsigned long long i);
   //The fastest i64toa fuction
   static
 #if defined(_WIN32)
@@ -36,7 +56,7 @@ extern "C" {
 #else
   inline __attribute__((always_inline))
 #endif
-  unsigned char i64toa(char* c, long long i) {
+  char* i64toa(char* c, long long i) {
   if (i < 0) { *c = 45; return u64toa(++c, ~--i); } return u64toa(c, i);
   }
 #ifdef __cplusplus
